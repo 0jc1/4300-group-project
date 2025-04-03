e74e281786c4d38484223f941150dbf920196117
0jc1 <epicdevelopers26@gmail.com>
2025-04-03 13:44:50 -0400

    Server routes

--- backend/db.ts
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const uri = "mongodb+srv://admin:<password>@cluster0.ntszore.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
